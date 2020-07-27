28811a27763fe0ba923db4de2fff14ee2774b427
root <root@5e9b4617fd56>
2020-07-27 23:56:51 +0000

    Working on googly eyes. Finish the up and down of the eyes

--- src/_02_boolean/_1_sleepy_head/SleepyHead.java
+++ src/_02_boolean/_1_sleepy_head/SleepyHead.java
@@ -12,28 +12,62 @@ public class SleepyHead {
 		/************************  SLEEPY HEAD  **************************/
 		boolean isWeekend;
 		// Write code to ask the user what day it is.
-		String day = JOptionPane.showInputDialog("What day is it today?");
+		String day = JOptionPane.showInputDialog("Is today a weekend?");
         
 		// Set the boolean isWeekend based on the value they enter
+		if(day.equalsIgnoreCase("yes")) {
+			isWeekend = true ;
+		}
+		else if(day.equalsIgnoreCase("no")) {
+			isWeekend = false ;
+		}
 		
+		else {
+			isWeekend = false;
+		}
 		
 		// If it is the weekend, tell the user they get to sleep in.
+		if(isWeekend == true) {
+			JOptionPane.showMessageDialog(null, "You can sleep in today");
+		}
 		
 		// If it is not the weekend, tell them to get out of bed and go to school!
+		if(isWeekend == false) {
+			JOptionPane.showMessageDialog(null, "Get out of bed and go to school");
+		}
 		
 		
 		/************************   STAR STUDENT  **************************/
 
 		boolean passedExam;
 		// Write code to ask the user what percentage they scored in their last exam
+		String grade = JOptionPane.showInputDialog("What percentage did you score in your exam?");
+		double gradeNum = Double.parseDouble(grade);
 
 		
 		// If they scored more than 70, they passed the exam.
 		// Set the boolean passedExam based on their score.
+		if(gradeNum > 70) {
+			passedExam = true;
+		}
+		
+		else if(gradeNum <= 70) {
+			passedExam = false;
+		}
+		
+		else {
+			passedExam = false;
+		}
 		
 		// If the user passed the exam, congratulate them
+		if(passedExam == true) {
+			JOptionPane.showMessageDialog(null, "Congratulation!!!");
+		}
 		
 		// otherwise, wish them better luck next time.
+		if(passedExam == false) {
+			JOptionPane.showMessageDialog(null, "Better luck next time");
+		}
 		
 		
 		/************************  GAME OVER  **************************/
@@ -50,16 +84,33 @@ public class SleepyHead {
 
 		}
 		// Tell the user "game is over" 
+		if(gameIsOver == true) {
+			JOptionPane.showMessageDialog(null, "Game is over");
+		}
 		
 		
 		/***********************   RED SQUARE  ********************/
 		
 		boolean isRed;
 		// Ask the user what color to draw with. Based on their answer, set the isRed variable
+		String color = JOptionPane.showInputDialog("What color should I draw with?");
+		if(color.equalsIgnoreCase("red")) {
+			isRed = true;
+		}
+		else {
+			isRed = false;
+		}
 		
 		
 		boolean isSquare;		
 		// Now ask the user what shape to draw. Based on their answer, set the isSquare variable
+		String shape = JOptionPane.showInputDialog("What shape should I draw ?");
+		if(shape.equalsIgnoreCase("square")) {
+			isSquare = true;
+		}
+		else {
+			isSquare = false;
+		}
 		
 		
 		// Now you MUST use the && operator to join the booleans for this code. 
@@ -67,12 +118,33 @@ public class SleepyHead {
 		// Use an if statement to ONLY draw a red square when it has been requested (use the method below)
 		// otherwise, tell the user you don't know how to draw that shape
 		
+		if(isRed == true && isSquare == true) {
+			drawRedSquare();
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "I do not know how to draw that shape");
+		}
+		
 
 	}
 
 	
 	static void drawRedSquare() {
-		Robot rob = new Robot();
+		Robot rob = new Robot("mini");
 		// Complete the rest of this method
+		rob.penDown();
+		rob.setSpeed(100);
+		rob.setPenColor(255, 0, 0);
+		
+		rob.move(30);
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(90);
+		rob.move(30);
+			
+		
 	}	
 }
--- src/_02_boolean/_2_googly_eyes/googly_eyes/googly_eyes.pde
+++ src/_02_boolean/_2_googly_eyes/googly_eyes/googly_eyes.pde
@@ -1,8 +1,34 @@
-
+PImage face;
 void setup() {
+face = loadImage("peppapig.png");
+size(800,600);
+face.resize(800,600);
   
 }
 
 void draw() {
+  background(face);
+  fill(255,255,255);
+  ellipse(365,150,40,40);
+  fill(0,0,0);
+
+  
+  fill(255,255,255);
+  ellipse(412,175,40,40);
+  fill(0,0,0);
+ 
+  
+  if( mouseX >  365 + 40/2 || mouseX<  365 - 40/2){
+     ellipse(365,150,15,15);
+     ellipse(365+47,150+25,15,15);
+   
+  } else{
+    ellipse(mouseX,mouseY,15,15);
+     ellipse(mouseX+47,mouseY+25,15,15);
+  }
+  
+  
+  
+
   
 }
